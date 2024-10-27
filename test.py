97309c7b40e7cb4bd4bb92445cbc6471619138cc
Andrew Braun <andrew.braun@csuglobal.edu>
2024-10-26 21:21:16 -0500

    Encapsulation 1

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-# Instance Attributes
-
-import random
+#Encapsulation
 
 class MyClass(object):
-    def dothis(self):
-        self.rand_val = random.randint(1,10)
+    def set_val(self, val): # Setter method
+        self.value = val
 
-myinst = MyClass()
-myinst.dothis()
-print(myinst.rand_val)
+    def get_val(self): # Getter method
+        return self.value
+
+a = MyClass()
+b = MyClass()
+
+a.set_val(10)
+b.set_val(100)
+
+a.value = 'hello'
+
+print(a.get_val())
+print(b.get_val())
